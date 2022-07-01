0c55f4d01f14ef364c51ee18c03aaf26b2f6dfa9
Juan Holguin <juholguin@unal.edu.co>
2022-06-30 19:22:31 -0500

    correct ingest_data.py - fresh transform_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -16,12 +16,12 @@ def ingest_data():
     import urllib.request
     for i in range(1995, 2022):
         try:
-            url = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/' + str(i) + '.xlsx'
+            url = 'https://github.com/jdvelasq/datalabs/raw/master/datasets/precio_bolsa_nacional/xls/' + str(i) + '.xlsx'
             file_ = 'data_lake/landing/' + str(i) + '.xlsx'
             print(url, file_)
             urllib.request.urlretrieve(url, file_)
         except:
-            url = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/' + str(i) + '.xls'
+            url = 'https://github.com/jdvelasq/datalabs/raw/master/datasets/precio_bolsa_nacional/xls/' + str(i) + '.xls'
             file_ = 'data_lake/landing/' + str(i) + '.xls'
             print(url, file_)
             urllib.request.urlretrieve(url, file_)
--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -7,10 +7,37 @@ def transform_data():
     H23.
 
     """
-    raise NotImplementedError("Implementar esta función")
-
+    import pandas as pd
+    # Transforma de 1995 a 1999 excluyendo las 3 primeras filas
+    for i in range(1995, 2000):
+        file_ = 'data_lake/raw/' + str(i) + '.csv'
+        path = "data_lake/landing/" + str(i) + ".xlsx"
+        df = pd.read_excel(path, skiprows=range(1, 3), header=1)
+        df.to_csv(file_, sep=',', encoding='utf-8')
+        print ("transform: ", path, "to", file_)
+    # Transforma de 2000 a 2016 excluyendo las 2 primeras filas
+    for i in range(2000, 2016):
+        file_ = 'data_lake/raw/' + str(i) + '.csv'
+        path = "data_lake/landing/" + str(i) + ".xlsx"
+        df = pd.read_excel(path, skiprows=range(1, 2), header=1)
+        df.to_csv(file_, sep=',', encoding='utf-8')
+        print("transform: ", path, "to", file_)
+    # Transforma de 2016 y 2017 solo los unicos xls
+    for i in range(2016, 2018):
+        file_ = 'data_lake/raw/' + str(i) + '.csv'
+        path = "data_lake/landing/" + str(i) + ".xls"
+        df = pd.read_excel(path, skiprows=range(1, 2), header=1)
+        df.to_csv(file_, sep=',', encoding='utf-8')
+        print("transform: ", path, "to", file_)
+    # Transforma de 2018 a 2022 sin exclusion
+    for i in range(2018, 2022):
+        file_ = 'data_lake/raw/' + str(i) + '.csv'
+        path = "data_lake/landing/" + str(i) + ".xlsx"
+        df = pd.read_excel(path)
+        df.to_csv(file_, sep=',', encoding='utf-8')
+        print("transform: ", path, "to", file_)
 
 if __name__ == "__main__":
     import doctest
-
+    transform_data()
     doctest.testmod()

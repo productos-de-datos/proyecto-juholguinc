72f2ac1967b41d3ef121f31d5a517843f9102579
juan holguin <juholguinc@unal.edu.co>
2022-07-01 20:17:28 -0500

    install openpyxl xlrd - plotday dev

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -7,10 +7,14 @@ def make_daily_prices_plot():
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
 
     """
-    raise NotImplementedError("Implementar esta función")
-
+    import pandas as pd
+    df_m = pd.read_csv('data_lake/business/precios-diarios.csv', index_col=0)
+    ax =df_m['precio'].plot(figsize=(15,5), kind='line', grid=True, x_compat=False)
+    fig = ax.get_figure()
+    fig.savefig('data_lake/business/reports/figures/daily_prices.png')
+    print("Saved ---")
 
 if __name__ == "__main__":
     import doctest
-
+    make_daily_prices_plot()
     doctest.testmod()
